8e0d6f1add900468ba4418938725b3c67beaaa30
ScottUlv <ulv15@protonmail.com>
2022-12-14 16:38:43 +0800

    Text animation Script implemented

---
+++ Final Year Project Why you kill it/Assets/Script/GameManager/ComicManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComicManager : MonoBehaviour
+{
+    [SerializeField] int letter;
+    [SerializeField] TextMeshProUGUI dialogBox;
+
+    public GameObject Page1;
+    public GameObject Page2;
+    public int keypressed = 0;
+
+    public void ChangePage1(){
+        Page1.SetActive(false);
+        Page2.SetActive(true);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Dialog("Dylen is missing."));
+    }
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.E)){
+            ChangePage1();
+            
+        }
+
+
+    }
+
+    public IEnumerator Dialog(string dialog){
+        dialogBox.text = "";
+
+        foreach (var text in dialog)
+        {
+            dialogBox.text += text;
+            yield return new WaitForSeconds(1f / letter);
+        }
+    }
+}
